1ccfbc5903281af25eb78b9ad3c800cf303d848c
Linus Alpsten <linus.alpsten@telia.com>
2017-02-08 11:47:52 +0100

    Added server and index

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res){
+    res.sendfile(__dirname + '/' + 'index.html');
+});
+
+var server = app.listen(8090, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('listening at %s %s', host, port);
+});
