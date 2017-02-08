115c7143feb118e4b3e96e2ebb5d13985899aa50
Linus Alpsten <linus.alpsten@telia.com>
2017-02-08 12:21:14 +0100

    Added server response to notindex.html

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', function(req,res){
     res.sendfile(__dirname + '/' + 'index.html');
 });
 
+app.get('/notindex', function(req,res){
+    res.sendfile(__dirname + '/' + 'notindex.html');
+});
+
 var server = app.listen(8090, function(){
     var host = server.address().address;
     var port = server.address().port;
